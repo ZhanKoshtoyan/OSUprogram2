1cffbb7ccee61eb67c38d5f3ca958d70f14fa4e7
koshtoian <koshtoian@tehnogr.ru>
2023-11-01 17:56:17 +0300

    Добавил record UserInput. Этот класс будет представлять перечень входных данных от пользователя. Тип данных double, т.к. В.Портянихин сказал что от Web API эта программа будет получать строки json.

---
+++ Libraries/UserInput.cs
@@ -0,0 +1,17 @@
+﻿// OSUprogram2
+// (c) Владимир Портянихин, 2023. Все права защищены.
+// Несанкционированное копирование этого файла
+// с помощью любого носителя строго запрещено.
+// Конфиденциально.
+
+namespace Libraries;
+
+public record UserInput
+{
+    public required double VolumeFlow { get; init; }
+    public required double TotalPressure { get; init; }
+    public required double TotalPressureDeviation { get; init; }
+    public required double Temperature { get; init; }
+    public required double RelativeHumidity { get; init; }
+    public required double Altitude { get; init; }
+}
