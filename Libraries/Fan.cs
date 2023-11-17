3cc173c7ee4f221d032423c5f6399c74f0f275e1
koshtoian <koshtoian@tehnogr.ru>
2023-11-17 14:27:17 +0300

    1. Образование имени переместил в отдельный класс FormationOfTheFanName.cs

--- Libraries/Fan.cs
+++ Libraries/Fan.cs
@@ -77,39 +77,7 @@ public class Fan
             userInput.Altitude = 20;
         }
 
-        var selectedTemperatureFan =
-            userInput.FanOperatingTemperature == 0
-                ? "ххх"
-                : userInput.FanOperatingTemperature.ToString();
-        var selectedSize = userInput.Size == 0
-            ? "ххх"
-            : userInput.Size.ToString()!.PadLeft(3, '0');
-        var selectedCaseLength =
-            userInput.FanBodyLength == 0 ? "х" : userInput.FanBodyLength.ToString();
-        var selectedImpellerRotationDirection =
-            userInput.ImpellerRotationDirection == "" ? "RRO" : userInput.ImpellerRotationDirection;
-        var selectedNominalPower =
-            userInput.NominalPower == 0
-                ? "хххх"
-                : ((int) userInput.NominalPower * 100).ToString().PadLeft(4, '0');
-        var selectedCaseMaterial =
-            userInput.CaseExecutionMaterial == "" ? "хх" : userInput.CaseExecutionMaterial;
-        var roundedImpellerRotationSpeed =
-            userInput.ImpellerRotationSpeed == 0
-                ?
-                data.NominalImpellerRotationSpeed.ToString("0000")
-                : userInput.ImpellerRotationSpeed.ToString("0000");
-
-        Name = string.Format(
-            "ОСУ-ДУ.{0}.{1}.{2}.{3}.{4}.{5}.{6}.Y2",
-            selectedTemperatureFan,
-            selectedSize,
-            selectedCaseLength,
-            selectedImpellerRotationDirection,
-            selectedNominalPower,
-            roundedImpellerRotationSpeed,
-            selectedCaseMaterial
-        );
+        Name = FormationOfTheFanName.DoIt(data, userInput);
 
         _air = new HumidAir().WithState(
             InputHumidAir.Altitude(userInput.Altitude.Meters()),
---
+++ Libraries/FormationOfTheFanName.cs
@@ -0,0 +1,42 @@
+﻿namespace Libraries;
+
+public static class FormationOfTheFanName
+{
+    public static string DoIt(FanData data, UserInput userInput)
+    {
+
+        var selectedTemperatureFan =
+            userInput.FanOperatingTemperature == 0
+                ? "ххх"
+                : userInput.FanOperatingTemperature.ToString();
+        var selectedSize = userInput.Size == 0
+            ? "ххх"
+            : userInput.Size.ToString()!.PadLeft(3, '0');
+        var selectedCaseLength =
+            userInput.FanBodyLength == 0 ? "х" : userInput.FanBodyLength.ToString();
+        var selectedImpellerRotationDirection =
+            userInput.ImpellerRotationDirection == "" ? "RRO" : userInput.ImpellerRotationDirection;
+        var selectedNominalPower =
+            userInput.NominalPower == 0
+                ? "хххх"
+                : ((int) userInput.NominalPower * 100).ToString().PadLeft(4, '0');
+        var selectedCaseMaterial =
+            userInput.CaseExecutionMaterial == "" ? "хх" : userInput.CaseExecutionMaterial;
+        var roundedImpellerRotationSpeed =
+            userInput.ImpellerRotationSpeed == 0
+                ?
+                data.NominalImpellerRotationSpeed.ToString("0000")
+                : userInput.ImpellerRotationSpeed.ToString("0000");
+
+        return string.Format(
+            "ОСУ-ДУ.{0}.{1}.{2}.{3}.{4}.{5}.{6}.Y2",
+            selectedTemperatureFan,
+            selectedSize,
+            selectedCaseLength,
+            selectedImpellerRotationDirection,
+            selectedNominalPower,
+            roundedImpellerRotationSpeed,
+            selectedCaseMaterial
+        );
+    }
+}
